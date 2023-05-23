2f7cbce75721b91ea24a8c9baa5c5c73d161ba06
AbbasBayramzade <abbas.bayramzade@mail.ru>
2023-05-23 17:39:02 +0400

    Add Footer Section

---
+++ js/credits.js
@@ -0,0 +1,19 @@
+// ********************** CREDITS NAV&TABS SECTION ******************************
+
+let tabsCrdt = document.querySelectorAll('.crdt_tab')
+
+let tabContentsCrdt = document.querySelectorAll('.tab_content_crdts')
+
+tabsCrdt.forEach(tab=>{
+  tab.addEventListener('click', ()=>{
+    let target = document.querySelector(tab.dataset.tabTarget)
+    tabContentsCrdt.forEach(tabContent=>{
+      tabContent.classList.remove('active')
+    })
+    tabsCrdt.forEach(tab =>{
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
